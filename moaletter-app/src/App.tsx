9af6e94fcc3057762d6f866036f6807f86afba44
yejz0715 <yejz0715@gmail.com>
2023-12-18 19:13:50 +0900

    Feat : vite로 프로젝트 생성 + 초기설정

---
+++ moaletter-app/src/App.tsx
@@ -0,0 +1,11 @@
+import AppRouter from "./AppRouter";
+
+/**
+ * 애플리케이션 진입점
+ * @returns{JSX.Element} App 컴포넌트
+ */
+const App = (): JSX.Element => {
+    return <AppRouter />;
+};
+
+export default App;
