78141267d608a46f9ba69ba012ea419293ec1069
송현주 <shyunju7@naver.com>
2023-12-19 20:15:46 +0900

    Style : Template 컴포넌트 UI 수정 및 Home 페이지 컴포넌트 구조 변경

--- moaletter-app/src/pages/Home.tsx
+++ moaletter-app/src/pages/Home.tsx
@@ -1,39 +1,23 @@
-import React from "react";
-import { Link } from "react-router-dom";
 import Template from "../components/Template";
+import BottomNavigation from "../components/BottomNavigation";
 
 /**
- * 홈 페이지
+ * 모아레터 애플리케이션 Home 페이지
  * @returns {JSX.Element} Home 페이지
- *
  */
 const Home = (): JSX.Element => {
-    return (
-        <Template>
-            <main>
-                <h1>home</h1>
-                <ul>
-                    <li>
-                        <Link to="/plan/details/:detailid">상세보기</Link>
-                    </li>
-
-                    <li>
-                        <Link to="/plan/register/letter">편지작성</Link>
-                    </li>
-                </ul>
-                <div>
-                    <Link to="/">
-                        <label>홈</label>
-                    </Link>
-                    <Link to="/plan/register">
-                        <label>+</label>
-                    </Link>
-                    <Link to="/letterbox/:letterid">
-                        <label>우편함</label>
-                    </Link>
-                </div>
-            </main>
-        </Template>
-    );
+  return (
+    <Template>
+      <>
+        <div className="my-6 mx-4">
+          <header>
+            <h2 className="font-SUITE-BD text-zinc-800 text-xl">모아레터</h2>
+          </header>
+          <section></section>
+        </div>
+        <BottomNavigation />
+      </>
+    </Template>
+  );
 };
 export default Home;
