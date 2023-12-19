1f22de930d670b4633984d62765097aa8a973f3d
yejz0715 <yejz0715@gmail.com>
2023-12-19 14:17:25 +0900

    Feact : 페이지 처리를 위한 라우트 생성

--- moaletter-app/src/AppRouter.tsx
+++ moaletter-app/src/AppRouter.tsx
@@ -1,46 +1,78 @@
-import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
-import BottomNavigation from "./components/BottomNavigation";
-import Home from "./pages/Home";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
-import MailBox from "./pages/MailBox";
+import Home from "./pages/Home";
 import PlanRegistration from "./pages/PlanRegistration";
-import PlanDetail from "./pages/PlanDetail";
-import LetterCreate from "./pages/LetterCreate";
+import PlanDetails from "./pages/PlanDetails";
+import LetterBox from "./pages/LetterBox";
+import LetterForm from "./pages/LetterForm";
+import { AuthRouteProps } from "./types/components";
 
 /**
- * Layout 입니다.
- * @returns {JSX.Element}
+ * 로그인이 필요한 페이지 처리를 위한 라우트
+ * @param {AuthRouteProps} props isLogin(로그인 여부) component(이동할 컴포넌트)
+ * @returns {JSX.Element} 이동할 컴포넌트
  */
-const Layout = (): JSX.Element => {
-    return (
-        <div>
-            <Outlet />
-            <BottomNavigation />
-        </div>
-    );
+
+const AuthRoute = ({ isLogin, component }: AuthRouteProps): JSX.Element => {
+    return isLogin ? component : <Navigate to="/login" replace />;
 };
 
 /**
- * AppRouter 입니다.
- * @returns {JSX.Element}
+ * 애플리케이션 라우터 관리
+ * @returns {JSX.Element} AppRouter 컴포넌트
  */
 const AppRouter = (): JSX.Element => {
-    const isLogin = false;
+    const isLogin = true;
 
     return (
         <BrowserRouter>
             <Routes>
                 {!isLogin && <Route path="/login" element={<Login />} />}
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                </Route>
-                <Route path="/plan/register" element={<PlanRegistration />} />
+
+                <Route
+                    path="/"
+                    element={
+                        <AuthRoute isLogin={isLogin} component={<Home />} />
+                    }
+                />
+
+                <Route
+                    path="/plan/register"
+                    element={
+                        <AuthRoute
+                            isLogin={isLogin}
+                            component={<PlanRegistration />}
+                        />
+                    }
+                />
+
                 <Route
                     path="/plan/details/:detailid"
-                    element={<PlanDetail />}
+                    element={
+                        <AuthRoute
+                            isLogin={isLogin}
+                            component={<PlanDetails />}
+                        />
+                    }
                 />
-                <Route path="/mailbox/:mailid" element={<MailBox />} />
-                <Route path="/plan/letter/create" element={<LetterCreate />} />
+                <Route
+                    path="/letterbox/:letterid"
+                    element={
+                        <AuthRoute
+                            isLogin={isLogin}
+                            component={<LetterBox />}
+                        />
+                    }
+                />
+                <Route
+                    path="/plan/register/letter"
+                    element={
+                        <AuthRoute
+                            isLogin={isLogin}
+                            component={<LetterForm />}
+                        />
+                    }
+                />
             </Routes>
         </BrowserRouter>
     );
