bda625b14b8a804c5cf2950d1ec3906452194e83
송현주 <shyunju7@naver.com>
2023-12-19 19:29:14 +0900

    Style : 반응형 레이아웃 적용

--- moaletter-app/src/components/Template.tsx
+++ moaletter-app/src/components/Template.tsx
@@ -1,15 +1,13 @@
-import React from "react";
-
 /**
  * 레이아웃 템플릿
  * @param {JSX.Element} props children(컴포넌트)
  * @returns {JSX.Element} Template 컴포넌트
  */
 const Template = ({ children }: { children: JSX.Element }): JSX.Element => {
-    return (
-        <main className="w-full h-full md:min-w-[640px] sm:max-w-[300px] bg-slate-100">
-            {children}
-        </main>
-    );
+  return (
+    <main className="max-[600px]:w-screen h-screen min-[600px]:min-w-[600px] bg-slate-100 py-3 px-2 overflow-y-auto">
+      {children}
+    </main>
+  );
 };
 export default Template;
