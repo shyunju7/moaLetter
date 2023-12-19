917599424fbd55b766f0b2614074e34db063371d
송현주 <shyunju7@naver.com>
2023-12-19 20:06:40 +0900

    Style : 애플리케이션 폰트 스타일 설정 및 tailwind.config에 폰트 스타일 등록

--- moaletter-app/tailwind.config.js
+++ moaletter-app/tailwind.config.js
@@ -1,13 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-        extend: {},
-        screens: {
-            sm: "640px",
-            md: "768px",
-            lg: "1024px",
-        },
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "SUITE-RG": ["SUITE-RG", "sans"],
+        "SUITE-MD": ["SUITE-MD", "sans"],
+        "SUITE-BD": ["SUITE-BD", "sans"],
+        "SUITE-HY": ["SUITE-HY", "sans"],
+      },
     },
-    plugins: [],
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+    },
+  },
+  plugins: [],
 };
