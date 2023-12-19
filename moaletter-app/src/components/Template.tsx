1ca75c839eb64f8e8e3cdb70d7845b39341e7c94
송현주 <shyunju7@naver.com>
2023-12-19 19:32:50 +0900

    Style : Template 컴포넌트 배경색 변경

--- moaletter-app/src/components/Template.tsx
+++ moaletter-app/src/components/Template.tsx
@@ -5,7 +5,7 @@
  */
 const Template = ({ children }: { children: JSX.Element }): JSX.Element => {
   return (
-    <main className="max-[600px]:w-screen h-screen min-[600px]:min-w-[600px] bg-slate-100 py-3 px-2 overflow-y-auto">
+    <main className="max-[600px]:w-screen h-screen min-[600px]:min-w-[600px] bg-white py-3 px-2 overflow-y-auto">
       {children}
     </main>
   );
