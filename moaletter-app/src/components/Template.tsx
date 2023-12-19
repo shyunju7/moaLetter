03b098d753157304fde445af70045b58f78f9fb8
송현주 <shyunju7@naver.com>
2023-12-19 19:29:53 +0900

    Comment : 주석 내용 변경

--- moaletter-app/src/components/Template.tsx
+++ moaletter-app/src/components/Template.tsx
@@ -1,6 +1,6 @@
 /**
- * 레이아웃 템플릿
- * @param {JSX.Element} props children(컴포넌트)
+ * 페이지 레이아웃을 담당할 컴포넌트
+ * @param {JSX.Element} props children(자식 컴포넌트)
  * @returns {JSX.Element} Template 컴포넌트
  */
 const Template = ({ children }: { children: JSX.Element }): JSX.Element => {
