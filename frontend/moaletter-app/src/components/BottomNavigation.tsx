5d244b057253bad3fcc7f7c3f3ee18dd913cdc53
yejz0715 <yejz0715@gmail.com>
2023-12-23 01:20:32 +0900

    Style : 하단 네브 바 아이콘 스타일 수정

--- frontend/moaletter-app/src/components/BottomNavigation.tsx
+++ frontend/moaletter-app/src/components/BottomNavigation.tsx
@@ -9,17 +9,19 @@ import { IoAddCircle } from "@react-icons/all-files/io5/IoAddCircle";
 const BottomNavigation = (): JSX.Element => {
     return (
         <nav className="flex items-center w-full max-w-[600px] h-[72px] bg-slate-50 fixed bottom-0">
-            <IoAddCircle className="absolute inset-1/4 top-[-45px] w-[50px] h-[50px] m-auto cursor-pointer" />
+            <IoAddCircle className="absolute inset-1/4 top-[-45px] w-[50px] h-[50px] m-auto cursor-pointer hover:text-[#CD6D6D]" />
             <ul className="flex justify-around items-center w-full ">
                 <li className="cursor-pointer">
                     <Link to="/">
-                        <IoHomeOutline className="w-[22px] h-[20px]" />
-                        <label className="ml-1 text-sm">홈</label>
+                        <IoHomeOutline className="w-[22px] h-[20px] text-[#B31312]" />
+                        <label className="ml-1 text-sm text-[#B31312]">
+                            홈
+                        </label>
                     </Link>
                 </li>
-                <li className="cursor-pointer">
+                <li className="cursor-pointer hover:text-[#CD6D6D]">
                     <Link to="/letters">
-                        <IoMailOutline className="w-[25px] h-[20px] m-auto" />
+                        <IoMailOutline className="w-[25px] h-[20px] m-auto " />
                         <label className="text-sm">우편함</label>
                     </Link>
                 </li>
