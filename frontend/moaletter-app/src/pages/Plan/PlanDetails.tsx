7faf6a054fb04c8730ad407e559bc4a35307375f
송현주 <shyunju7@naver.com>
2023-12-25 19:12:37 +0900

    feat : 모아모아 상세 페이지 헤더 적용 및 테스트

--- frontend/moaletter-app/src/pages/Plan/PlanDetails.tsx
+++ frontend/moaletter-app/src/pages/Plan/PlanDetails.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 import Template from "../../components/Template";
+import Header from "../../components/Header";
 
 /**
  * 모아모아의 상세 페이지
  * @returns {JSX.Element} PlanDetails 페이지
  */
 const PlanDetails = (): JSX.Element => {
-  return (
-    <Template>
-      <div className="my-6 mx-4">
-        <h1>상세페이지를 볼 수 있어요</h1>
-      </div>
-    </Template>
-  );
+	return (
+		<Template>
+			<Header title="모아모아 상세" />
+		</Template>
+	);
 };
 export default PlanDetails;
