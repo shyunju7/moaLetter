65aed2923a9ab1faf5a875266143b5b9621cbda2
yejz0715 <yejz0715@gmail.com>
2023-12-25 20:29:44 +0900

    Refactor : 옵셔널 체이닝 적용

--- frontend/moaletter-app/src/components/CardList.tsx
+++ frontend/moaletter-app/src/components/CardList.tsx
@@ -7,39 +7,31 @@ import { Link } from "react-router-dom";
  * @returns {JSX.Element} CardList 컴포넌트
  */
 const CardList = (): JSX.Element => {
-    return (
-        <section className="w-full max-w-[361px] h-full mx-3 border-l border-[#999999] pt-[15px]">
-            {planList &&
-                planList.map((plan: planListType, index: number) => (
-                    <div
-                        key={plan.id}
-                        className="relative w-full h-[101px] mb-[30px]"
-                    >
-                        <ul className="absolute top-[-25px] left-[-5px] flex items-center">
-                            <li>
-                                <div className=" w-[10px] h-[10px]  bg-black rounded-full"></div>
-                            </li>
-                            <li>
-                                <label className="font-SUITE-BD text-[12px] text-[#666666] ml-[11px] mr-[2px]">
-                                    오후
-                                </label>
-                                <label className="font-SUITE-BD text-[14px]">
-                                    2시50분
-                                </label>
-                            </li>
-                        </ul>
-                        <Link to="/plan/:planId">
-                            <Card plan={plan} />
-                        </Link>
-                        {index === planList.length - 1 ? (
-                            <div className="absolute top-[122px] left-[-5px] w-[10px] h-[10px]  bg-black"></div>
-                        ) : (
-                            ""
-                        )}
-                    </div>
-                ))}
-        </section>
-    );
+  return (
+    <section className="w-full max-w-[361px] h-full mx-3 border-l border-[#999999] pt-[15px]">
+      {planList?.map((plan: planListType, index: number) => (
+        <div key={plan.id} className="relative w-full h-[101px] mb-[30px]">
+          <ul className="absolute top-[-25px] left-[-5px] flex items-center">
+            <li>
+              <div className=" w-[10px] h-[10px]  bg-black rounded-full"></div>
+            </li>
+            <li>
+              <label className="font-SUITE-BD text-[12px] text-[#666666] ml-[11px] mr-[2px]">
+                오후
+              </label>
+              <label className="font-SUITE-BD text-[14px]">2시50분</label>
+            </li>
+          </ul>
+          <Link to="/plan/:planId">
+            <Card plan={plan} />
+          </Link>
+          {index === planList.length - 1 && (
+            <div className="absolute top-[122px] left-[-5px] w-[10px] h-[10px]  bg-black"></div>
+          )}
+        </div>
+      ))}
+    </section>
+  );
 };
 export default CardList;
 
