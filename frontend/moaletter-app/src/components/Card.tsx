215e85c8fbd2e8c30c2f6eab9aae3c601a4ce2df
yejz0715 <yejz0715@gmail.com>
2023-12-25 20:19:12 +0900

    Style : 카드 상태 색상 변경

--- frontend/moaletter-app/src/components/Card.tsx
+++ frontend/moaletter-app/src/components/Card.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import people from "../assets/ic-people.svg";
+import peopleImage from "../assets/ic-people.svg";
 import { planListType } from "../data/dummy";
 
 /**
@@ -8,34 +8,34 @@ import { planListType } from "../data/dummy";
  * @returns {JSX.Element} Card 컴포넌트
  */
 const Card = ({ plan }: { plan: planListType }): JSX.Element => {
-    const getStatusColor = [
-        "bg-[#d9d9d9] text-[#666666]",
-        "bg-[#C0403F] text-[#ffffff]",
-        "bg-[#DB9B9A] text-[#ffffff]",
-    ];
-    const getStatusText = ["종료", "진행중", "예정"];
+  const statusColor = [
+    "bg-[#d9d9d9] text-[#666666]",
+    "bg-[#ffa500] text-[#ffffff]",
+    "bg-[#6eb5ff] text-[#ffffff]",
+  ];
+  const statusText = ["종료", "진행중", "예정"];
 
-    return (
-        <article className="flex flex-col justify-between w-full h-[76px] mx-[14px] p-3 bg-[#fafafa] border border-[#f4f4f4] rounded-[10px]">
-            <ul className="flex justify-between items-end">
-                <li>
-                    <label className="font-SUITE-BD">{plan.roomName}</label>
-                </li>
-                <li
-                    className={`w-[38px] h-[17px] rounded-sm leading-[18px] text-center text-[10px] ${
-                        getStatusColor[plan.status]
-                    } `}
-                >
-                    {getStatusText[plan.status]}
-                </li>
-            </ul>
-            <div className="flex gap-1">
-                <img src={people} className="w-3" />
-                <label className="text-xs text-[#666666]">
-                    {plan.attendeesCount}/{plan.allParticipants.length}
-                </label>
-            </div>
-        </article>
-    );
+  return (
+    <article className="flex flex-col justify-between w-full h-[76px] mx-[14px] p-3 bg-[#fafafa] border border-[#f4f4f4] rounded-[10px]">
+      <ul className="flex justify-between items-end">
+        <li>
+          <label className="font-SUITE-BD">{plan.roomName}</label>
+        </li>
+        <li
+          className={`w-[38px] h-[17px] rounded-sm leading-[18px] text-center text-[10px] ${
+            statusColor[plan.status]
+          } `}
+        >
+          {statusText[plan.status]}
+        </li>
+      </ul>
+      <div className="flex gap-1">
+        <img src={peopleImage} className="w-3" />
+        <label className="text-xs text-[#666666]">
+          {plan.attendeesCount}/{plan.allParticipants.length}
+        </label>
+      </div>
+    </article>
+  );
 };
 export default Card;
