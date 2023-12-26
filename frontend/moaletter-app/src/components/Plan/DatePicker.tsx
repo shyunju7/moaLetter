b44af04752ea0b353ce1afd6af1c7d134087d987
yejz0715 <yejz0715@gmail.com>
2023-12-27 01:37:05 +0900

    Comment : DatePicker 컴포넌트 주석 내용 추가

--- frontend/moaletter-app/src/components/Plan/DatePicker.tsx
+++ frontend/moaletter-app/src/components/Plan/DatePicker.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { IoCalendarOutline } from "@react-icons/all-files/io5/IoCalendarOutline";
-const DatePicker = () => {
+
+/**
+ * 날짜와 시간을 선택하는 컴포넌트
+ * @returns {JSX.Element} DatePicker 컴포넌트
+ */
+const DatePicker = (): JSX.Element => {
   return (
     <div className=" flex items-center w-full max-w-[360px] h-[43px] pl-3 py-[14px] rounded-[10px] border border-[#F4F4F4]">
       <IoCalendarOutline size="16px" />
