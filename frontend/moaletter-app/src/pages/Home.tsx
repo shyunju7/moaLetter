38eaf9cb3f13fb035fa2e6c8c5219200a6055439
송현주 <shyunju7@naver.com>
2023-12-26 22:21:39 +0900

    feat : Home 내부 알림 아이콘 클릭시 알림 페이지로 이동하게 로직 설정

--- frontend/moaletter-app/src/pages/Home.tsx
+++ frontend/moaletter-app/src/pages/Home.tsx
@@ -3,28 +3,31 @@ import BottomNavigation from "../components/BottomNavigation";
 import { IoNotificationsOutline } from "@react-icons/all-files/io5/IoNotificationsOutline";
 import CardList from "../components/CardList";
 import PlanCalendar from "../components/PlanCalendar";
+import { Link } from "react-router-dom";
 /**
  * 모아레터 애플리케이션 Home 페이지
  * @returns {JSX.Element} Home 페이지
  */
 const Home = (): JSX.Element => {
-  return (
-    <Template>
-      <>
-        <div className="my-6 mx-4">
-          <header className="flex justify-between">
-            <h2 className="font-SUITE-BD text-zinc-800 text-xl">모아레터</h2>
-            <IoNotificationsOutline className="cursor-pointer w-5 h-5 hover:text-[#CD6D6D]" />
-          </header>
+	return (
+		<Template>
+			<>
+				<div className="my-6 mx-4">
+					<header className="flex justify-between">
+						<h2 className="font-SUITE-BD text-zinc-800 text-xl">모아레터</h2>
+						<Link to="/notice">
+							<IoNotificationsOutline className="cursor-pointer w-5 h-5 hover:text-[#CD6D6D]" />
+						</Link>
+					</header>
 
-          <section className="flex flex-col justify-center items-center">
-            <PlanCalendar />
-            <CardList />
-          </section>
-        </div>
-        <BottomNavigation />
-      </>
-    </Template>
-  );
+					<section className="flex flex-col justify-center items-center">
+						<PlanCalendar />
+						<CardList />
+					</section>
+				</div>
+				<BottomNavigation />
+			</>
+		</Template>
+	);
 };
 export default Home;
