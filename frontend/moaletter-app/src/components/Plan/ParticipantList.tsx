3004d5cbd1c727299e626392c28eb7764bccb8da
yejz0715 <yejz0715@gmail.com>
2023-12-26 19:03:02 +0900

    Feat : ParticipantList 컴포넌트 생성

---
+++ frontend/moaletter-app/src/components/Plan/ParticipantList.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Participant from "./Participant";
+
+const ParticipantList = () => {
+  return (
+    <div className="w-full max-w-[360px] h-full border border-[#f4f4f4] rounded-[10px]">
+      <Participant />
+    </div>
+  );
+};
+export default ParticipantList;
