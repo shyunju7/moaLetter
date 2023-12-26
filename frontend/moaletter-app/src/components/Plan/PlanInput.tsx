b4f6a20a1161243d8a3b5b1a2d4e00ec48fe695c
yejz0715 <yejz0715@gmail.com>
2023-12-26 19:04:08 +0900

    Feat : PlanInput 컴포넌트 생성

---
+++ frontend/moaletter-app/src/components/Plan/PlanInput.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PlanInput = () => {
+  return (
+    <input
+      type="text"
+      placeholder="모임의 이름을 적어주세요."
+      className="w-full max-w-[360px] h-[45px] p-[12px] bg-[#f4f4f4] rounded-[10px] text-[14px] text-[#666666] focus:outline-none"
+    />
+  );
+};
+export default PlanInput;
