d293fd10291d787893c884742a6e5083dc2ea880
yejz0715 <yejz0715@gmail.com>
2023-12-27 01:49:42 +0900

    Feat :  선택한 참여자 항목에 onClick 함수와 스타일 적용 & Comment : Participant 컴포넌트 주석 내용 추가

--- frontend/moaletter-app/src/components/Plan/Participant.tsx
+++ frontend/moaletter-app/src/components/Plan/Participant.tsx
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { FriendListType } from "../../data/dummy";
 
-const Participant = () => {
+/**
+ * 참여자를 등록할 항목을 나타내는 컴포넌트
+ * @param {FriendListType} friend 친구 목록에서 친구 항목 객체
+ * @returns {JSX.Element} Participant 컴포넌트
+ */
+const Participant = ({ friend }: { friend: FriendListType }): JSX.Element => {
+  const [isSelected, setIsSelected] = useState(false);
+
   return (
-    <>
-      <div className="flex justify-between items-center w-full max-w-[360px] h-[68px] border-b border-black px-3">
-        <div className="flex items-center">
-          <div className="w-9 h-9 border rounded-full bg-gray-100 mr-[14px]"></div>
-          <label>이예지</label>
-        </div>
-        <div className="w-4 h-4 border border-[#2F2F2] bg-black rounded-full"></div>
+    <section
+      onClick={() => setIsSelected((prev) => !prev)}
+      className={`flex justify-between items-center w-full max-w-[360px] h-[68px] border-b border-[#f4f4f4] px-3 cursor-pointer`}
+    >
+      <div className="flex items-center">
+        <div className="w-9 h-9 bg-[#D9D9D9] rounded-full mr-[14px]"></div>
+        <label className="text-[14px] text-[#333333]">{friend.name}</label>
       </div>
-    </>
+      <div
+        className={`w-4 h-4  rounded-full border-2 border-[#F2F2F2] ${
+          isSelected && `bg-[#C0403F]`
+        }`}
+      ></div>
+    </section>
   );
 };
 export default Participant;
