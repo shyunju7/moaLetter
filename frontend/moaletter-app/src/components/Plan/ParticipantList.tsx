72fed7b982bf846bf50c2feaa357249156380306
yejz0715 <yejz0715@gmail.com>
2023-12-27 01:47:08 +0900

    Feat :  ParticipantList 컴포넌트에 친구 목록 매핑을 위한 map 함수 적용 & Comment : ParticipantList 컴포넌트 주석 내용 추가

--- frontend/moaletter-app/src/components/Plan/ParticipantList.tsx
+++ frontend/moaletter-app/src/components/Plan/ParticipantList.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Participant from "./Participant";
+import { friendList } from "../../data/dummy";
 
-const ParticipantList = () => {
+/**
+ * 참여자를 등록하는 목록
+ * @returns {JSX.Element} ParticipantList 컴포넌트
+ */
+const ParticipantList = (): JSX.Element => {
   return (
-    <div className="w-full max-w-[360px] h-full border border-[#f4f4f4] rounded-[10px]">
-      <Participant />
+    <div className="w-full max-w-[360px] h-full mb-[54px] border border-[#f4f4f4] rounded-[10px]">
+      {friendList?.map((friend) => (
+        <Participant key={friend.id} friend={friend} />
+      ))}
     </div>
   );
 };
