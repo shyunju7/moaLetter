45b47dec298f205585fd1a2830b421139811b0f5
yejz0715 <yejz0715@gmail.com>
2023-12-27 01:25:41 +0900

    Comment : Button 컴포넌트 주석 내용 추가

--- frontend/moaletter-app/src/components/Button.tsx
+++ frontend/moaletter-app/src/components/Button.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+/**
+ * 버튼 컴포넌트
+ * @param {string} text(버튼에 표시될 텍스트)
+ * @returns {JSX.Element} Button 컴포넌트
+ */
 const Button = ({ text }: { text: string }): JSX.Element => {
   return (
     <button className="w-full max-w-[360px] h-[54px] mb-10 rounded-[10px] bg-[#C0403F] text-[#ffffff]">
