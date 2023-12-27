319c9926240ea34fb0b053bc9de7d85e2a7645bd
yejz0715 <yejz0715@gmail.com>
2023-12-27 15:12:48 +0900

    Style : 하단 네브바 아이콘 + 스타일 변경

--- frontend/moaletter-app/src/components/BottomNavigation.tsx
+++ frontend/moaletter-app/src/components/BottomNavigation.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
-import { IoHomeOutline } from "@react-icons/all-files/io5/IoHomeOutline";
-import { IoMailOutline } from "@react-icons/all-files/io5/IoMailOutline";
+import { IoHome } from "@react-icons/all-files/io5/IoHome";
+import { IoMail } from "@react-icons/all-files/io5/IoMail";
 import { IoAddCircle } from "@react-icons/all-files/io5/IoAddCircle";
 /**
  * 하단 네비게이션 바를 담당할 컴포넌트
@@ -10,16 +10,18 @@ const BottomNavigation = (): JSX.Element => {
   return (
     <nav className="flex items-center w-full max-w-[600px] h-[72px] bg-slate-50 fixed bottom-0">
       <ul className="flex justify-around items-center w-full relative">
-        <li className="cursor-pointer">
+        <li className="cursor-pointer hover:text-[#cdcdcd]">
           <Link to="/">
-            <IoHomeOutline className="w-[22px] h-[20px] text-[#B31312]" />
+            <IoHome className="w-[22px] h-[20px] text-[#B31312]" />
             <label className="ml-1 text-sm text-[#B31312]">홈</label>
           </Link>
         </li>
-        <li className="cursor-pointer hover:text-[#CD6D6D]">
+        <li className=" group cursor-pointer">
           <Link to="/letters">
-            <IoMailOutline className="w-[25px] h-[20px] m-auto" />
-            <label className="text-sm">편지함</label>
+            <IoMail className="w-[25px] h-[20px] m-auto text-[#666666] group-hover:text-[#cd6d6d]" />
+            <label className="text-sm  text-[#666666] group-hover:text-[#cd6d6d]">
+              편지함
+            </label>
           </Link>
         </li>
         <Link
--- frontend/moaletter-app/src/pages/Home.tsx
+++ frontend/moaletter-app/src/pages/Home.tsx
@@ -9,25 +9,25 @@ import { Link } from "react-router-dom";
  * @returns {JSX.Element} Home 페이지
  */
 const Home = (): JSX.Element => {
-	return (
-		<Template>
-			<>
-				<div className="my-6 mx-4">
-					<header className="flex justify-between">
-						<h2 className="font-SUITE-BD text-zinc-800 text-xl">모아레터</h2>
-						<Link to="/notice">
-							<IoNotificationsOutline className="cursor-pointer w-5 h-5 hover:text-[#CD6D6D]" />
-						</Link>
-					</header>
+  return (
+    <Template>
+      <>
+        <div className="my-6 mx-4">
+          <header className="flex justify-between">
+            <h2 className="font-SUITE-BD text-zinc-800 text-xl">모아레터</h2>
+            <Link to="/notice">
+              <IoNotificationsOutline className="cursor-pointer w-5 h-5 hover:text-[#CD6D6D]" />
+            </Link>
+          </header>
 
-					<section className="flex flex-col justify-center items-center">
-						<PlanCalendar />
-						<CardList />
-					</section>
-				</div>
-				<BottomNavigation />
-			</>
-		</Template>
-	);
+          <section className="flex flex-col justify-center items-center">
+            <PlanCalendar />
+            <CardList />
+          </section>
+        </div>
+        <BottomNavigation />
+      </>
+    </Template>
+  );
 };
 export default Home;
