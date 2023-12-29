e75716abdd54f14d5eddcadcfe15c9ca16f200f1
yejz0715 <yejz0715@gmail.com>
2023-12-29 19:40:36 +0900

    Feat : Button onClick함수 생성

--- frontend/moaletter-app/src/components/Button.tsx
+++ frontend/moaletter-app/src/components/Button.tsx
@@ -2,12 +2,30 @@ import React from "react";
 
 /**
  * 버튼 컴포넌트
- * @param {string} text(버튼에 표시될 텍스트)
+ * @param {string} props text(버튼에 표시될 텍스트) registerEvent(버튼 클릭 이벤트 핸들러)
  * @returns {JSX.Element} Button 컴포넌트
  */
-const Button = ({ text }: { text: string }): JSX.Element => {
+type handleButtonType = {
+  [id: string]: string | string[] | Date;
+};
+
+const Button = ({
+  text,
+  registerEvent,
+}: {
+  text: string;
+  registerEvent: () => handleButtonType;
+}): JSX.Element => {
+  const handleButton = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(registerEvent());
+    e.preventDefault();
+  };
   return (
-    <button className="w-full max-w-[360px] h-[54px] mb-10 rounded-[10px] bg-[#C0403F] text-[#ffffff]">
+    <button
+      type="submit"
+      onClick={handleButton}
+      className="w-full max-w-[360px] h-[54px] mb-10 rounded-[10px] bg-[#C0403F] text-[#ffffff]"
+    >
       {text}
     </button>
   );
