74be08b7d7f2f0abdd7bd551a06d1e2e1e4fbb7c
yejz0715 <yejz0715@gmail.com>
2024-01-10 16:50:06 +0900

    Feact : LetterForm 컴포넌트 ui 및 스타일 적용

--- frontend/moaletter-app/src/pages/Letter/LetterForm.tsx
+++ frontend/moaletter-app/src/pages/Letter/LetterForm.tsx
@@ -1,15 +1,40 @@
+import { useState } from "react";
 import Header from "../../components/Header";
 import Template from "../../components/Template";
+import Canvas from "../../components/Canvas/Canvas";
+import LetterContents from "../../components/Letter/LetterContents";
 
 /**
  * 편지를 작성하는 페이지
  * @returns {JSX.Element} LetterForm 페이지
  */
 const LetterForm = (): JSX.Element => {
-	return (
-		<Template>
-			<Header title="편지 작성" />
-		</Template>
-	);
+  const [isLetter, setIsLetter] = useState<boolean>(true);
+
+  const changeButton = (): void => {
+    setIsLetter((prev) => !prev);
+  };
+  return (
+    <Template>
+      <div>
+        <Header title="편지 작성" />
+        <section className="flex flex-col items-center gap-2 w-full max-w-[361px] h-full m-auto mt-5">
+          <div className="flex justify-end gap-2 w-full">
+            <button
+              onClick={changeButton}
+              className="w-full max-w-[76px] h-8 rounded-[10px] bg-[#cd6d6d] text-xs text-white"
+            >
+              {isLetter ? "그림그리기" : "편지쓰기"}
+            </button>
+            <button className="w-full max-w-[76px] h-8 rounded-[10px] bg-[#cd6d6d] text-xs text-white">
+              저장하기
+            </button>
+          </div>
+
+          {isLetter ? <LetterContents /> : <Canvas />}
+        </section>
+      </div>
+    </Template>
+  );
 };
 export default LetterForm;
